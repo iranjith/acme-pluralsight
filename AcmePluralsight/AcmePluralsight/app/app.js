ace75f948eaf34675f4561807527284b9478a58d
Ranjith kumar <ranjith.kumar3@in.ey.com>
2017-09-14 17:45:40 +0530

    Routing and Welcome page added.

--- AcmePluralsight/AcmePluralsight/app/app.js
+++ AcmePluralsight/AcmePluralsight/app/app.js
@@ -7,4 +7,32 @@
                            ["common.services",
                             "ui.router",
                             "productResourceMock"]);
+
+
+    app.config(["$stateProvider",
+               "$urlRouterProvider",
+       function ($stateProvider, $urlRouterProvider) {
+           $urlRouterProvider.otherwise("/");
+
+           $stateProvider
+               .state("home", {
+                   url: "/",
+                   templateUrl: "app/welcomeView.html"
+               })
+               // Products
+               .state("productList", {
+                   url: "/products",
+                   templateUrl: "app/products/productListView.html",
+                   controller: "ProductListCtrl as vm"
+               })
+               .state("productEdit", {
+                   url: "/products/edit/:productId",
+                   templateUrl: "app/products/productEditView.html",
+                   controller: "ProductEditCtrl as vm"
+               })
+
+       }]
+   );
+
+
 }());
