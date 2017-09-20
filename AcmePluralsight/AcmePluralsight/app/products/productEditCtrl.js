aa228520a523ec0242190927ce7e151f259e6965
Ranjith kumar <ranjith.kumar3@in.ey.com>
2017-09-20 08:13:37 +0530

    Service related changes.

--- AcmePluralsight/AcmePluralsight/app/products/productEditCtrl.js
+++ AcmePluralsight/AcmePluralsight/app/products/productEditCtrl.js
@@ -9,19 +9,42 @@
         .controller("ProductEditCtrl",
         ["product",
             "$state",
+            "productService",
             ProductEditCtrl]);
 
 
-    function ProductEditCtrl(product, $state) {
+    function ProductEditCtrl(product, $state, productService) {
         var vm = this;
 
         vm.product = product;
+        vm.priceOption = "percent";
+
+        vm.marginPercent = function () {
+            return productService.calculateMarginPercent(vm.product.price,
+                vm.product.cost)
+        };
+
+        /* Calculate the price based on a markup */
+        vm.calculatePrice = function () {
+            var price = 0;
+
+            if (vm.priceOption == 'amount') {
+                price = productService.calculatePriceFromMarkupAmount(
+                    vm.product.cost, vm.markupAmount);
+            }
+
+            if (vm.priceOption == 'percent') {
+                price = productService.calculatePriceFromMarkupPercent(
+                    vm.product.cost, vm.markupPercent);
+            }
+            vm.product.price = price;
+        };
 
         if (vm.product && vm.product.productId) {
             vm.title = "Edit: " + vm.product.productName;
         }
         else {
-            vm.title = "New Product"
+            vm.title = "New Product";
         }
 
         vm.open = function ($event) {
@@ -31,16 +54,19 @@
             vm.opened = !vm.opened;
         };
 
-        vm.submit = function () {
-            vm.product.$save(function (data) {
-                toastr.success("Save Successful");
+        vm.submit = function (isValid) {
+            if (isValid) {
+                vm.product.$save(function (data) {
+                    toastr.success("Save Successful");
+                })
+            } else {
+                alert("Please correct the validation errors first.");
             }
-            );
-        }
+        };
 
         vm.cancel = function () {
             $state.go('productList');
-        }
+        };
 
         vm.addTags = function (tags) {
             if (tags) {
@@ -50,11 +76,10 @@
             } else {
                 alert("Please enter one or more tags separated by commas");
             }
-        }
+        };
 
         vm.removeTag = function (idx) {
             vm.product.tags.splice(idx, 1);
-        }
-
+        };
     }
 }());
